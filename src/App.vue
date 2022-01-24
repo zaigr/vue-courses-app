3243786d07d8311409556cd947f48a7a6831d349
Ihar Zakharau <ihar_zakharau@epam.com>
2022-01-24 14:02:15 +0300

    Add courses component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
   <Header />
+  <Courses />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+
 import Header from '@/components/Header.vue'
+import Courses from '@/views/Courses.vue'
 
 export default defineComponent({
   components: {
-    Header
+    Header,
+    Courses
   }
 })
 
 </script>
+
+<style>
+body {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+}
+
+input {
+    border-style: solid;
+    border-width: small;
+    outline: none;
+}
+
+</style>
---
+++ src/views/Courses.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class='action-bar'>
+        <SearchBar placeholder='Enter course name...' />
+    </div>
+    <main>
+        <CourseCard v-for='course in courses' :course='course' :key='course.id' />
+    </main>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue'
+
+import { DataSource, Course } from '@/helpers/DataSource'
+
+import SearchBar from '@/components/common/SearchBar.vue'
+import CourseCard from '@/components/CourseCard.vue'
+
+export default defineComponent({
+  components: {
+    CourseCard,
+    SearchBar
+  },
+  data () {
+    return {
+      courses: [] as Course[]
+    }
+  },
+  mounted () {
+    this.courses = DataSource.GetCourses()
+  }
+})
+</script>
+
+<style scoped>
+.action-bar {
+    margin: 2%;
+}
+</style>
