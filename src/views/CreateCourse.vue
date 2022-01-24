630801348ad38972d2b8a472f75a8afa92b93d63
Ihar Zakharau <ihar_zakharau@epam.com>
2022-01-24 15:49:35 +0300

    Add create course view

---
+++ src/views/CreateCourse.vue
@@ -0,0 +1,40 @@
+<template>
+    <main>
+        <section>
+            <label>
+                Title
+                <input type='text' v-model='courseName' placeholder='Enter course name...' />
+            </label>
+            <label>
+                Description
+                <input type='textarea' v-model='courseDescription' placeholder='Enter description.. ' />
+            </label>
+            <Button text='Create course' />
+        </section>
+        <section>
+            <h3>Add author</h3>
+            <label>
+                Author name
+                <input type='text' placeholder='Enter author name...' />
+            </label>
+        </section>
+    </main>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue'
+
+import Button from '@/components/common/Button.vue'
+
+export default defineComponent({
+  components: {
+    Button
+  },
+  data () {
+    return {
+      courseName: '',
+      courseDescription: ''
+    }
+  }
+})
+</script>
