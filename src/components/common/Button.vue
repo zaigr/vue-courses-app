262b2b4584e84a3fe307d7c44ed2dcc3057ff87c
Ihar Zakharau <ihar_zakharau@epam.com>
2022-01-24 10:09:56 +0300

    Add header component

---
+++ src/components/common/Button.vue
@@ -0,0 +1,32 @@
+<template>
+    <button class='btn'>{{ text }}</button>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    text: {
+      type: String,
+      default: (): string => 'Click'
+    }
+  }
+})
+</script>
+
+<style>
+.btn {
+    width: 160px;
+    height: 40px;
+    text-decoration: none;
+    font-size: medium;
+    border-style: solid;
+    border-width: small;
+    border-color: purple;
+    background-color: white;
+    color: black;
+    cursor: pointer;
+    user-select: none;
+}
+</style>
